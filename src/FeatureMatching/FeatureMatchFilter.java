8640b2a9440d39632f51ae51595079fd937a0451
peter laudel <peter.laudel@gmail.com>
2014-06-03 15:39:29 +0200

    Changed: Standard Vector do List<Matches> interface

--- src/FeatureMatching/FeatureMatchFilter.java
+++ src/FeatureMatching/FeatureMatchFilter.java
@@ -1,16 +1,17 @@
 package FeatureMatching;
 
+import java.util.List;
 import java.util.Vector;
 
 public class FeatureMatchFilter {
 	
-	public static Vector<Matches> DoRatioTest(Vector<Vector<Matches>> knnMatches)
+	public static List<Matches> DoRatioTest(List<List<Matches>> knnMatches)
 	{
-		Vector<Matches> resultMatches = new Vector<Matches>();
+		List<Matches> resultMatches = new Vector<Matches>();
 
 		for(int i = 0; i< knnMatches.size(); i++)
 		{
-			Vector<Matches> tmpMatches = knnMatches.get(i);
+			List<Matches> tmpMatches = knnMatches.get(i);
 			if(tmpMatches.size() > 1)
 			{
 				if(tmpMatches.get(0).distance / tmpMatches.get(1).distance < 0.8f)
